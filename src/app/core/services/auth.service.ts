ad8e622f2b742e1ccb57a7cd221bd96275cb708d
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-02-24 12:27:16 -0300

    feat(Criado função de login no AuthService): Função de login criada

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,12 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private apiUrl: string = "http://tarefas.safererp.com.br/redmine";
+
   constructor(
-    private http: HttpClient
+    private http: HttpClient,
   ) { }
+
+  login(username: string, password: string): Observable<any> {
+    const authAuthorization = 'Basic ' + btoa(`${username}:${password}`)
+
+    const headers = new HttpHeaders({
+      'Authorization': authAuthorization,
+      'Content-Type': 'application/json',
+    })
+
+    return this.http.get(`${this.apiUrl}/users/current.json`, { headers, withCredentials: true },);
+  }
+
 }
