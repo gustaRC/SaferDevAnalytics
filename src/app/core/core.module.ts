dba0aadc66f1dbc4e42a4842c7a6227ece79fe6f
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-02-25 19:08:42 -0300

    feat(Criado componente de login): Criado componente login para implementação futura

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
     CommonModule,
     RouterModule,
