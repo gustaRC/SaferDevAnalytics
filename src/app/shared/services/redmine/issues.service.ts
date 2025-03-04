6f75363410e6c5711c68526a813b93b993c7c94d
Gustavo Rodrigues <gustavorcprofissional@gmail.com>
2025-03-04 11:58:09 -0300

    feat(Implementado funções de buscar issues): Criado função para buscar as issues e buscar todas as issues definindo offset e limit

--- src/app/shared/services/redmine/issues.service.ts
+++ src/app/shared/services/redmine/issues.service.ts
@@ -1,6 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable, Injector } from '@angular/core';
 import { BaseService } from '../base.service';
+import { expand, map, Observable, reduce } from 'rxjs';
+import { FilterIssues } from '../../models/filter-issues.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,6 +15,28 @@ export class IssuesService extends BaseService {
       'issues',
       injector
     )
+  }
+
+  searchIssues(filter: FilterIssues): Observable<any> {
+    return this.http.get(`${this.baseUrl}.json?${this.buildRedmineFilterUrl(filter)}`);
+  }
+
+  getAllIssues(filter: FilterIssues): Observable<any[]> {
+    const limit = 100;
+    const offset = 0;
+    const filters = { ...filter, limit, offset };
+
+    return this.searchIssues(filters)
+    .pipe(
+      expand((response: any) => {
+          filters.offset += limit;
+          return filters.offset < response.total_count ? this.searchIssues(filters) : [];
+        }
+      ),
+      map((response: any) => response.issues),
+      reduce((acc, issues) => acc.concat(issues), [])
+    );
+
   }
 
 }
