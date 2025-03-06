19bf590336a25957dc2e332a5ab8e4722eeca01a
Gustavo Rodrigues <123694354+gustaRC@users.noreply.github.com>
2025-03-06 12:59:27 -0300

    feat(Adicionado busca de grupos no GroupsService): Iniciado implementação do GroupService

--- src/app/shared/services/redmine/groups.service.ts
+++ src/app/shared/services/redmine/groups.service.ts
@@ -1,10 +1,15 @@
-import { Injectable, Injector } from '@angular/core';
+import { Injectable, Injector, signal } from '@angular/core';
 import { BaseService } from '../base.service';
+import { GroupUsers } from '../../models/group-user/group-users.model';
+import { BaseModel } from '../../models/base.model';
+import { map, Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupsService extends BaseService {
+
+  readonly groupsWithUsers = signal<GroupUsers[]>([]);
 
   constructor(
     protected override injector: Injector
@@ -14,5 +19,28 @@ export class GroupsService extends BaseService {
       injector
     )
   }
+
+  async getGroupsWithUsers() {
+    await this.getGroups()
+  }
+
+  searchGroups(): Observable<BaseModel[]> {
+    return this.http.get<{groups: BaseModel[]}>(`${this.baseUrl}.json`)
+    .pipe(
+      map((response: {groups: BaseModel[]}) => response.groups)
+    );
+  }
+
+  //PRIVATE METHODS
+
+  private async getGroups(): Promise<void> {
+    await this.searchGroups()
+    .toPromise().then(
+      (responseGroups: any) => {
+        this.setIdsResources(responseGroups);
+      }
+    );
+  }
+
 
 }
